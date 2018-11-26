dda5bbcafde2f72be6dc6af5143a63a293fb28ad
Alina Fesyk <fesyk.alina@gmail.com>
2018-11-26 15:00:41 +0000

    add a division operation
    Add a member-function which performs division of two float point
    digits.
    
    Signed-off-by: Alina Fesyk <fesyk.alina@gmail.com>

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,13 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+int Calculator::Div (double a, double b)
+{
+    if (!b)
+    {
+        std::cout << "Error: dividing by 0" << std::endl;
+        return -1;
+    }
+    return a / b + 0.5;
+}
